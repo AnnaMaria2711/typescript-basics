3a343d69c3e66c48030758d59eafc90c2870b725
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-02-28 11:06:28 +0100

    improved and added code to test.ts

--- test.ts
+++ test.ts
@@ -9,11 +9,24 @@ console.log(filteredArray);
 const sumOfArray = array.reduce((accumulator,currentValue)=>{
     return accumulator * currentValue;
 },0);
-console.log(sumOfArray)
+console.log(sumOfArray);
 
 const initialValue = 0;
 let accumulator = initialValue;
-array.forEach(currentValue=> accumulator = accumulator+currentValue)
+array.forEach(currentValue=> accumulator = accumulator+currentValue);
 
 const checkForNumberBiggerThan10 = array.some(n=> n >= 10);
-console.log(checkForNumberBiggerThan10)
+console.log(checkForNumberBiggerThan10);
+
+const liste = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+
+const sortedArray= liste.sort((a,b)=> {
+    return b-a;
+
+});
+
+const squaredNumbers= sortedArray.map(n=> n*n);
+
+ squaredNumbers.splice(squaredNumbers.length - 4, 4);
+ squaredNumbers.splice(0 , 1)
+console.log(squaredNumbers);
